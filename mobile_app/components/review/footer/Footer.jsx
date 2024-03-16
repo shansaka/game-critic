340e3863845c8cea57ed0107556baabca982babc
Salindu Hansaka <hansaka.official@gmail.com>
2024-03-16 17:46:51 +0000

    Refactor review footer component and add location data

--- mobile_app/components/review/footer/Footer.jsx
+++ mobile_app/components/review/footer/Footer.jsx
@@ -1,50 +1,78 @@
 import { View, Text, TouchableOpacity, Image, Linking } from "react-native";
-import { useRouter, useLocalSearchParams } from 'expo-router'
+import { useCallback, useState } from "react";
+import { useRouter, useLocalSearchParams, useFocusEffect } from "expo-router";
 import styles from "./footer.style";
 import { icons } from "../../../constants";
-import useFetch from '../../../hook/useFetch';
+import useFetch from "../../../hook/useFetch";
 import inputValidator from "../../../helpers/inputValidator";
+import * as Location from "expo-location";
 
 const Footer = ({ gameId, title, comments, rating, setTitle, setComments }) => {
   const router = useRouter();
   const params = useLocalSearchParams();
+  const [locationData, setLocationData] = useState(false);
+
   const requiresAuth = true;
-  const { data , isLoading, error, refetch, totalPages, fetchData } = useFetch(`reviews`, null, false, {
-    gameId: gameId,
-    title: title,
-    comments: comments,
-    rating: rating,
-    location: "location",
-  }, requiresAuth);
+  const { data, isLoading, error, refetch, totalPages, fetchData } = useFetch(
+    `reviews`,
+    null,
+    false,
+    {
+      gameId: gameId,
+      title: title,
+      comments: comments,
+      rating: rating,
+      location: locationData,
+    },
+    requiresAuth
+  );
+
+  useFocusEffect(
+    useCallback(() => {
+      const fetchLocation = async () => {
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== "granted") {
+          alert("Permission to access location was denied");
+          return;
+        }
+
+        let location = await Location.getCurrentPositionAsync({});
+        let reverseGeocode = await Location.reverseGeocodeAsync(
+          location.coords
+        );
+        let city = reverseGeocode[0].city;
+        let country = reverseGeocode[0].country;
+        let latitude = location.coords.latitude;
+        let longitude = location.coords.longitude;
+        setLocationData({ city, country, latitude, longitude });
+      };
+
+      fetchLocation();
+    }, [])
+  );
 
   const onSubmitPressed = async () => {
-    const titleError = inputValidator(title, 'empty')
-    const commentsError = inputValidator(comments, 'empty')
+    const titleError = inputValidator(title, "empty");
+    const commentsError = inputValidator(comments, "empty");
 
     if (titleError || commentsError) {
-      setTitle({ ...title, error: titleError })
-      setComments({ ...comments, error: commentsError })
-      return
+      setTitle({ ...title, error: titleError });
+      setComments({ ...comments, error: commentsError });
+      return;
     }
 
     const responseData = await fetchData();
 
-    if(responseData){
+    if (responseData) {
       router.replace(`/gameDetails/${gameId}`);
+    } else {
+      alert("Please check your email and password.");
     }
-    else{
-        alert('Please check your email and password.');
-    }
-  }
+  };
 
   return (
     <View style={styles.container}>
-     
-
-      <TouchableOpacity
-        style={styles.applyBtn}
-        onPress={onSubmitPressed}
-      >
+      <TouchableOpacity style={styles.applyBtn} onPress={onSubmitPressed}>
         <Text style={styles.applyBtnText}>Submit</Text>
       </TouchableOpacity>
     </View>
--- web_api/models/review.js
+++ web_api/models/review.js
@@ -1,55 +1,67 @@
-const mongoose = require('mongoose');
-const User = require('./user');
-const Game = require('./game');
+const mongoose = require("mongoose");
+const User = require("./user");
+const Game = require("./game");
 
 const { Schema, model } = mongoose;
 
 const reviewSchema = new Schema({
-    user: {
-        type: Schema.Types.ObjectId,
-        ref: 'Users',
-        required: true
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "Users",
+    required: true,
+  },
+  game: {
+    type: Schema.Types.ObjectId,
+    ref: "Games",
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+  },
+  comments: {
+    type: String,
+    required: true,
+  },
+  location: {
+    city: {
+      type: String,
     },
-    game: {
-        type: Schema.Types.ObjectId,
-        ref: 'Games',
-        required: true
+    country: {
+      type: String,
     },
-    title: { 
+    latitude: {
+      type: Number,
+    },
+    longitude: {
+      type: Number,
+    },
+  },
+  status: {
+    type: String,
+    enum: ["Pending", "Approved", "Rejected"],
+    default: "Pending",
+  },
+  images: [
+    {
+      URL: {
         type: String,
-        required: true
+        required: true,
+      },
     },
-    rating: {
-        type: Number,
-        required: true
-    },
-    comments: {
-        type: String,
-        required: true
-    },
-    location: {
-        type: String,
-        required: true
-    },
-    status: {
-        type: String,
-        enum: ['Pending', 'Approved', 'Rejected'],
-        default: 'Pending'
-    },
-    images: [{
-		URL: {
-			type: String,
-			required: true
-		}
-	}],
-    dateCreated: {
-        type: Date,
-        default: Date.now
-    },
-    dateUpdated: {
-        type: Date,
-        default: Date.now
-    }
+  ],
+  dateCreated: {
+    type: Date,
+    default: Date.now,
+  },
+  dateUpdated: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-module.exports = model('Reviews', reviewSchema);
+module.exports = model("Reviews", reviewSchema);
