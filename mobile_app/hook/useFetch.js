4c154985a7ba7d48bd0c86c43bd11e4e8ebd0202
Salindu Hansaka <hansaka.official@gmail.com>
2024-03-22 19:00:00 +0000

    Add API URL constant to useFetch hook

--- mobile_app/hook/useFetch.js
+++ mobile_app/hook/useFetch.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import { useCallback, useState } from "react";
 import { getSessionItem, updateToken } from "../helpers/loginSession";
 
+// The API URL, Need to change if using localhost
 const apiUrl = "https://game-critic-web-api.onrender.com/api";
 
 const useFetch = (
