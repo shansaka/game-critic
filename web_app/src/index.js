ac21f5790b7b92cb3d6c9bde4d0aa5c64e5123a1
shansaka <hansaka.official@gmail.com>
2025-01-07 22:25:30 +0000

    Log API URL from environment variables in index.js

--- web_app/src/index.js
+++ web_app/src/index.js
@@ -4,6 +4,8 @@ import App from "./App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
+console.log("REACT_APP_API_URL:", process.env.REACT_APP_API_URL);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
